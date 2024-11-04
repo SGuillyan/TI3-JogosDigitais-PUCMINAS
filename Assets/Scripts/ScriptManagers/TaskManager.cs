bd89baa6751b3977090c26db5896ff6204536aa0
Said <160852484+SGuillyan@users.noreply.github.com>
2024-11-03 22:53:57 -0300

    UI Quest Responsiva

--- Assets/Scripts/ScriptManagers/TaskManager.cs
+++ Assets/Scripts/ScriptManagers/TaskManager.cs
@@ -5,7 +5,40 @@ using UnityEngine;
 public class TaskManager : MonoBehaviour
 {
     public List<Quest> quests;
-    public MoneyManager moneyManager; // Referência ao MoneyManager
+    public MoneyManager moneyManager;
+    public Canvas questCanvas;
+    public GameObject questUIPrefab;
+    public GameObject spawnTask;
+
+    void Start()
+    {
+        // Cria e adiciona uma nova Quest simples
+        Quest simpleQuest = new Quest
+        {
+            title = "Primeira Missão",
+            description = "Colete 5 recursos para completar esta missão.",
+            isCompleted = false,
+            objectives = new List<Objective>
+            {
+                new Objective
+                {
+                    description = "Coletar 5 recursos",
+                    isCompleted = false,
+                    type = ObjectiveType.Collect,
+                    targetAmount = 5,
+                    currentAmount = 0
+                }
+            },
+            reward = new Reward(moneyAmountFun: 100, ecologicoValorFun: 10, economicoValorFun: 5, socialValorFun: 8)
+        };
+
+        AddQuest(simpleQuest);
+    }
+
+    void OnEnable()
+    {
+        Instantiate(questUIPrefab, spawnTask.transform);
+    }
 
     public void AddQuest(Quest newQuest)
     {
@@ -26,5 +59,15 @@ public class TaskManager : MonoBehaviour
         {
             quest.reward.GiveReward(moneyManager);
         }
+    }
+
+    public void ShowQuestCanvas()
+    {
+        questCanvas.gameObject.SetActive(true); // Ativa o Canvas
+    }
+
+    public void HideQuestCanvas()
+    {
+        questCanvas.gameObject.SetActive(false); // Desativa o Canvas
     }
 }
