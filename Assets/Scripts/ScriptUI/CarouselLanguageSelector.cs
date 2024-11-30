c4139c998d35a6dee3f22546269fe6db002d224e
JordanLyon <jordandlyon@gmail.com>
2024-11-30 15:37:51 -0300

    Added new UI and more
    Adjusted the fonts and texts in all areas; Added the config and the credits UI;

--- Assets/Scripts/ScriptUI/CarouselLanguageSelector.cs
+++ Assets/Scripts/ScriptUI/CarouselLanguageSelector.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class CarouselLenguageSelector : MonoBehaviour
+public class CarouselLanguageSelector : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private Image image;
-    [SerializeField] private string[] lenguageList;
+    [SerializeField] private string[] languageList;
     [SerializeField] private Sprite[] flagSprits;
     private int page = 0;
 
@@ -18,21 +18,21 @@ public class CarouselLenguageSelector : MonoBehaviour
 
     void AtualizarPagina()
     {
-        text.text = lenguageList[page];
+        text.text = languageList[page];
         image.sprite = flagSprits[page];
     }
 
     public void Anterior()
     {
         if (page > 0) page -= 1;
-        else page = lenguageList.Length - 1;
+        else page = languageList.Length - 1;
 
         AtualizarPagina();
     }
 
     public void Proximo()
     {
-        if (page < lenguageList.Length - 1) page += 1;
+        if (page < languageList.Length - 1) page += 1;
         else page = 0;
 
         AtualizarPagina();
